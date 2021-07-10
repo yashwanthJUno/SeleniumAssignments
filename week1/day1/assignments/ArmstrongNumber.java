635fca368127eddfe0b9190cd0c15f4b46e96bb5
yashwanthJUno <87225353+yashwanthJUno@users.noreply.github.com>
2021-07-10 18:27:47 +0530

    Update ArmstrongNumber.java
    Output
    3  27     15
    5  152     1
    1  153     0
    The given Number is Armstrong Number153

--- week1/day1/assignments/ArmstrongNumber.java
+++ week1/day1/assignments/ArmstrongNumber.java
@@ -48,9 +48,27 @@ public class ArmstrongNumber {
 
 		// Declare your input
 
-		
+			int originalno = 153;
+			int remainder = 0;
+            int quotient = 0;
+			int new_num=originalno;
+            int calculate =0;
+	        while (new_num != 0)
+	        {
+	            remainder = new_num % 10;
+	            calculate = calculate+(remainder*remainder*remainder);
+	            new_num = new_num/10;
+	            System.out.println(remainder+"  "+calculate+"     "+new_num);
+	        }
 
-		
+	        if(calculate == originalno) {
+	            System.out.println("The given Number is Armstrong Number" + originalno); }
+	        else
+		{  
+			System.out.println(" it's not an Armstrong Number  "+originalno);
+		}
+
+	}
 
 			// Declare 3 more int variables as calculated, remainder, original (Tip: Initialize calculated alone)
 
