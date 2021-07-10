e5b5e2de98c7acea4b7bf090f08989125889a57f
yashwanthJUno <87225353+yashwanthJUno@users.noreply.github.com>
2021-07-10 21:37:53 +0530

    Update PrimeNumber.java
    13 is a Prime Number

--- week1/day1/assignments/PrimeNumber.java
+++ week1/day1/assignments/PrimeNumber.java
@@ -30,11 +30,11 @@ public class PrimeNumber {
 
 	 * What are my learnings from this code?
 
-	 * 1)
+	 * 1) boolean condition checked.
 
-	 * 2)
+	 * 2) Selection statements used inside the iterative statement.
 
-	 * 3) 
+	 * 3) break.
 
 	 * 
 
@@ -77,6 +77,36 @@ public class PrimeNumber {
 			
 
 			// Print 'Not a Prime' when the condition doesn't match 
+		 int inPut = 13;
+	  boolean to_check = false;
+	  int remainder =0;
+	  
+	  for(int i=2; i<=inPut/2; i++)
+	  {
+		  remainder = inPut%i;
+		  //remainder = 0;
+		 //System.out.println(remainder);
+		 if(remainder==0)
+		 {
+			 to_check=true;
+		 }
+		 else
+		 {
+			 to_check=false;
+			 //System.out.println("inside");
+		 }	 
+		 break;  
+	  }
+	  if(to_check==false)
+		 {
+			 System.out.println(inPut+" is a Prime Number");
+		 }
+		 else
+		 {
+			 System.out.println(inPut+" Not a Prime Number");
+		 }
+
+
 
    }
 
